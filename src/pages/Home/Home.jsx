24fb30fa4edcedbab2320c71d3bebae736137765
Michael Crowe <michael.crowe30@gmail.com>
2026-01-03 14:13:55 -0400

    updated stles for homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,33 +5,36 @@ import '/src/assets/styles/pages/_home.scss'
 const Home = () => {
 
     return (
-        <main className="page__left">
-            <div className='page__intro'>
-                _hi my name is
+        <main className="page">
+            <div className='page__left'>
+                <div className='page__left--intro'>
+                    _hi my name is
+                </div>
+
+                <div className='page__left--header'>
+                    ~ Michael Crowe
+                </div>
+
+                <div className='page__left--title'>
+                    [Cyber Security Professional] && [Junior Developler]
+                </div>
+
+                <div className='page__left--details'>
+                    <p>/Interests/ </p>
+                    <ul>
+                        <li>
+                            Cyber Security
+                        </li>
+                        <li>
+                            Web Development
+                        </li>
+                        <li>
+                            AI Development & Engineering (Learning)
+                        </li>
+                    </ul>
+                </div>
             </div>
 
-            <div className='page__header'>
-                ~ Michael Crowe
-            </div>
-
-            <div className='page__title'>
-                [Cyber Security Professional] && [Junior Developler]
-            </div>
-
-            <div className='page__details'>
-                <p>/Interests/ </p>
-                <ul>
-                    <li>
-                        Web Development
-                    </li>
-                    <li>
-                        Cyber Security
-                    </li>
-                    <li>
-                        AI Development (Learning)
-                    </li>
-                </ul>
-            </div>
 
         </main>
     )    
